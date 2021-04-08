5177283cb7d18d41dbc40e365c6951ff5bce18d2
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-04-08 12:27:26 +0100

    Changing portfolio

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,9 +2,9 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Mohamud Mussa', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Mohamud Mussa Portfolio Website', // e.g: Welcome to my website
 };
 
 // HERO DATA
@@ -18,7 +18,7 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'newlogo.png',
-  paragraphOne: '',
+  paragraphOne: 'asdad  ',
   paragraphTwo: '',
   paragraphThree: '',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
@@ -31,26 +31,34 @@ export const projectsData = [
     img: 'jar.PNG',
     title: 'Inventory Management System',
     info: 'For this project I created an Inventory management system written in Java. Using GCP to host a MySQL database, this project aimed at creating a system that can manipulate data on the cloud by allowing users to create, update, request the information or delete it. Planning process was thorough and included Agile methodology and the utilisation of JIRA boards. The development included a Java code to run the processes with Junit and Mockito as unit tests. The project included a completed project management board, including user stories, acceptance criteria which were estimations via story points, and prioritisation via MoSCoW methodology. This produced a working inventory-management system capable of tracking & manipulating customer’s information, orders details and item data. I then presented the project and explained the project process in detail.',
-    info2: 'Tech Used: Java, GCP, MySQL',
+    info2: 'Tech Used: Java, GCP, MySQL, Junit',
     repo: 'https://github.com/MohamudMussa/ims-demo', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'todo.jpg',
-    title: 'Full Stack ToDo List',
+    title: 'Full Stack (Java Spring Boot) ToDo List',
     info: 'For this project I created a full-stack web application which allows users to Create, Update and Delete their Tasks from their To-Do List. The structure of this project followed a three-tier architecture: the front end was built using visual studio code and written in JavaScript, HTML and CSS. The backend was built using Spring and written in Java and finally the locally hosted database was built using SQL. The application was then fully tested with Unit, Automated and Integration testing. Finally, it was linked to SonarQube to allow static analysis and improvement of the code.',
-    info2: '',
+    info2: 'Java Spring Boot, JavaScript, HTML, CSS',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/MohamudMussa/ToDoList-Project', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'projectThree.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    title: 'MERN - Cinema Project ',
+    info: 'I was part of a team that was tasked to create a full-stack web application using the MERN stack. The data layer was created using MongoDB with the back end being built using Node for the database and Express to construct an API. I tasked with setting up the initial backend using mongoDB and then worked heavily on the frontend which was built using React. I also lead the design aspect of the website, designing our custom images on Photoshop and ensuring that the interface works seamlessly. This web application was developed and implemented with the goal of providing an informative and visually appealing webpage for users to interact with. his project was a great experience for us to successfully incorporate an agile methodology and understand its benefits for application development.',
+    info2: 'Tech Used - React, MongoDB, Javascript',
+    repo: 'https://github.com/MohamudMussa/QACinemasProject', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'coonect.JPG',
+    title: 'Connect Communities',
+    info: 'I took part in a Hackathon that was aimed at building solutions to tackle homelessness with Crisis. My team came up with an idea that would Connect Communities around the country with a few simple steps. Connect Communitys mission is to connect people with the right community regardless of language barrier or location.The frontend was built using React, TailwindCSS and the backend was deployed with Firebase.We then develop a short and concise presentation to present back to the Crisis team',
+    info2: 'Tech Used - React, Firebase, TailwindCSS',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/deendevelopers/team-10-community-app', // if no repo, the button will not show up
   },
 ];
 
@@ -58,7 +66,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'mmussa92@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,12 +75,12 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'twitter.com/moremussa',
     },
     {
       id: nanoid(),
       name: 'codepen',
-      url: '',
+      url: 'https://www.linkedin.com/in/mohamud-mussa-76272083/',
     },
     {
       id: nanoid(),
@@ -89,5 +97,5 @@ export const footerData = {
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
