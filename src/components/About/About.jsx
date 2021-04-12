78c4c8b32823a404198701cd9a12f32966219c6f
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-04-12 14:20:45 +0100

    changed the CV

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -56,7 +56,7 @@ const About = () => {
                       className="cta-btn cta-btn--resume"
                       href={resume}
                     >
-                      Resume
+                      My CV
                     </a>
                   </span>
                 )}
