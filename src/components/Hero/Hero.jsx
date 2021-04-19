b7cfcd7dc67bbd9f85a710948abd5f71502044ba
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-04-19 19:50:54 +0100

    Revert "Update Hero.jsx"
    This reverts commit f7dce13db1c76d13f38c100b5466f351f4a8ce33.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -29,7 +29,7 @@ const Header = () => {
             {title || 'Hi, my name is'}{' '}
             <span className="text-color-main">{name || 'Mohamud Mussa'}</span>
             <br />
-            {subtitle || 'A designer turned fullstack development.'}
+            {subtitle || 'A Junior Full-Stack Developer.'}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
